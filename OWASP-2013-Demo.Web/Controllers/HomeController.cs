ce22a1817c463d926455bddf508411f572474627
AshleyPoole <git@ashleypoole.co.uk>
2015-07-13 20:02:57 +0100

    Adding HomeController and MVC structure

---
+++ OWASP-2013-Demo.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OWASP_2013_Demo.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
