2ba57bc07eb97cd7043bbc283ba3368f5b66c6b3
AshleyPoole <git@ashleypoole.co.uk>
2015-07-15 14:02:07 +0100

    Updating default controller

--- OWASP-2013-Demo.Web/App_Start/RouteConfig.cs
+++ OWASP-2013-Demo.Web/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace OWASP_2013_Demo.Web
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
