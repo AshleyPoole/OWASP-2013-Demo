bbb4d71f554e485b6ad41ede5baf7094ddf8dfa8
AshleyPoole <git@ashleypoole.co.uk>
2015-07-22 18:57:56 +0100

    Adding ConfigurationProvider and GoRedirector implementations

---
+++ Source/OWASP-2013-Demo.Domain/ConfigurationProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Configuration;
+using OWASP_2013_Demo.Interfaces;
+
+namespace OWASP_2013_Demo.Domain
+{
+	public class ConfigurationProvider : IConfigurationProvider
+	{
+		private string _allowedDomain;
+
+		public string GetAllowedDomain()
+		{
+			return _allowedDomain ?? (_allowedDomain = ConfigurationManager.AppSettings["AllowedDomain"]);
+		}
+	}
+}
---
+++ Source/OWASP-2013-Demo.Domain/GoRedirector.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using OWASP_2013_Demo.Interfaces;
+using OWASP_2013_Demo.Models;
+
+namespace OWASP_2013_Demo.Domain
+{
+	public class GoRedirector : IGoRedirectProvider
+	{
+		private readonly bool _secure;
+		private readonly IConfigurationProvider _configurationProvider;
+
+	    // Default to unsecure mode
+		public GoRedirector (IConfigurationProvider configurationProvider, bool secure = false)
+		{
+			_secure = secure;
+			_configurationProvider = configurationProvider;
+		}
+
+		public Redirect ProcessGoDirection(string url)
+		{
+			var uri = new Uri(url);
+			var redirectResponse = new Redirect() {Allowed = true, UrlForRedirect = uri};
+
+			if (!_secure) return redirectResponse;
+
+			// Secure mode activated
+			var allowedDomain = _configurationProvider.GetAllowedDomain();
+
+			if (uri.Host != allowedDomain)
+			{
+				redirectResponse.Allowed = false;
+				redirectResponse.ErrorMessage =
+					string.Format(
+						"Potentially dangerous redirect detected and blocked. Submitted url {0} did not match allowed domain list.", url);
+			}
+
+			return redirectResponse;
+		}
+	}
+}
