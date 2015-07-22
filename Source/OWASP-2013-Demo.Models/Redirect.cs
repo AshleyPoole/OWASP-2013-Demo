d3108e69512abecb7c819614bf62d0839f33e263
AshleyPoole <git@ashleypoole.co.uk>
2015-07-22 14:03:06 +0100

    Adding models project with Redirect model

---
+++ Source/OWASP-2013-Demo.Models/Redirect.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace OWASP_2013_Demo.Models
+{
+    public class Redirect
+    {
+	    public bool Allowed;
+	    public Uri UrlForRedirect;
+	    public string ErrorMessage;
+    }
+}
