5ca4089ff9a046f177cf59f2ee484a6bbf7d59ec
AshleyPoole <git@ashleypoole.co.uk>
2015-07-22 20:04:27 +0100

    Controller changes for redirection

---
+++ OWASP-2013-Demo.Web/Controllers/RedirectController.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Mvc;
+using OWASP_2013_Demo.Interfaces;
+using OWASP_2013_Demo.Web.Models;
+
+namespace OWASP_2013_Demo.Web.Controllers
+{
+    public class RedirectController : Controller
+    {
+	    private readonly IRedirectProvider _redirectProvider;
+
+	    public RedirectController(IRedirectProvider redirectProvider)
+	    {
+		    _redirectProvider = redirectProvider;
+	    }
+
+	    // GET: Go
+        public ActionResult Index(RedirectRequest redirectRequest)
+        {
+			var redirectResponse = _redirectProvider.ProcessGoDirection(redirectRequest.Url);
+
+			// Redirect client to requested url if allowed
+	        if (redirectResponse.Allowed) return Redirect(redirectResponse.UrlForRedirect.ToString());
+
+			// Else return the error
+	        redirectRequest.Error = redirectResponse.ErrorMessage;
+	        return Index(redirectRequest);
+        }
+    }
+}
