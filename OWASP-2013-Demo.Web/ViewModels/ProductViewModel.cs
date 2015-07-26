e252a2e6e1a6cc524fd704cbda5f59ff3b30c932
AshleyPoole <git@ashleypoole.co.uk>
2015-07-26 19:19:22 +0100

    Adding Product related view models

---
+++ OWASP-2013-Demo.Web/ViewModels/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OWASP_2013_Demo.Web.ViewModels
+{
+	public class ProductViewModel : BaseViewModel
+	{
+	}
+}
---
+++ OWASP-2013-Demo.Web/ViewModels/ProductsViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace OWASP_2013_Demo.Web.ViewModels
+{
+	public class ProductsViewModel : BaseViewModel
+	{
+		public IEnumerable<ProductViewModel> Products;
+	}
+}
