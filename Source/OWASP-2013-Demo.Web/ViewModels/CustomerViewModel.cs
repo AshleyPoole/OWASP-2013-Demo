c4f5430e9549416b439bb9af7f90d9b51ae17aac
Ashley Poole <git@ashleypoole.co.uk>
2015-08-01 16:44:33 +0100

    Adding concrete customer models

---
+++ Source/OWASP-2013-Demo.Web/ViewModels/CustomerViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace OWASP_2013_Demo.Web.ViewModels
+{
+	public class CustomerViewModel
+	{
+		public int CustomerID;
+		public int BusinessEntityID;
+		public string EmailAddress;
+		public string Title;
+		public string FirstName;
+		public string FullName;
+	}
+}
