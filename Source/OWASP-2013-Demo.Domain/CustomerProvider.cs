e4f3f91ffd742e982036f379c6099f3049aa8707
Ashley Poole <git@ashleypoole.co.uk>
2015-08-01 16:44:10 +0100

    Adding skeleton Customer provider

---
+++ Source/OWASP-2013-Demo.Domain/CustomerProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using OWASP_2013_Demo.Interfaces;
+
+namespace OWASP_2013_Demo.Domain
+{
+	class CustomerProvider : ICustomerProvider
+	{
+		public ICustomer GetCustomerByEmailAddress(string emailAddress)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
